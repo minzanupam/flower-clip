2abc247245631c39586bb0b50724a8875648bc23
anupam <anupamminz300am@gmail.com>
2025-02-11 23:46:17 +0530

    update schema include svgs table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
 	email TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL
 );
+
+CREATE TABLE svgs (
+	id INTEGER PRIMARY KEY autoincrement,
+	name TEXT NOT NULL,
+	file BLOB NOT NULL,
+	created_at TEXT NOT NULL
+	-- add user identifier later
+);
